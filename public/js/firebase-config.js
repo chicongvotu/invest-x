c69f3d35a2bb1582c8730f472bb913240cb0da42
Cong Nguyen <cong.nguyen@anfin.vn>
2026-08-15 10:59:30 +0700

    Fix: Move Firebase initialization to inline script before module imports

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,13 +1,4 @@
-// Firebase Configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDX-56B0mIqTKZdWBZx7rI0vkrGRKJNrBg",
-  authDomain: "invest-x-505513.firebaseapp.com",
-  projectId: "invest-x-505513",
-  storageBucket: "invest-x-505513.firebasestorage.app",
-  messagingSenderId: "829926289619",
-  appId: "1:829926289619:web:a1b2c3d4e5f6g7h8i9j0"
-};
-
-firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth();
-export const storage = firebase.storage();
+// Firebase is initialized in admin-news.html inline script
+// This module re-exports the global instances
+export const auth = window.auth;
+export const storage = window.storage;
